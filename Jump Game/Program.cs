7b7d906bbd205475ae3b97cf83736294ef931733
Emre <Emre@DESKTOP-779F2QA>
2023-08-11 16:17:13 +0300

    Proje dosyası ekle.

---
+++ Jump Game/Program.cs
@@ -0,0 +1,42 @@
+﻿public class Solution
+{
+    public bool CanJump(params int[] nums)
+    {
+        if (nums.Length == 1) return true;
+        if (nums[0] >= nums.Length - 1) return true;
+        {
+            int index = 0;
+            int value = nums[index];
+
+            int tempIndex = 0;
+            int tempValue = 0;
+
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if (value + index >= nums.Length - 1) return true;
+                for (int i = index + 1; i < index + value + 1; i++)
+                {
+                    if (i + nums[i]>value + index)
+                    {
+                        index = i; 
+                       value= nums[i];
+                        break;
+                    }
+
+                    if (value == 1) continue;
+                }
+                value = tempValue;
+                index = tempIndex;
+            }
+        }
+        return false;
+    }
+
+static void Main()
+{ 
+Solution s = new Solution();
+Console.WriteLine(s.CanJump(3, 0, 8, 2, 0, 0, 1));         
+}
+
+
+}
